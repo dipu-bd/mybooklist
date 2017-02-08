0d90edebdf38f914009a28e8f3bbd2ebd7269107
dipu-bd <dipu.sudipta@gmail.com>
2017-02-08 11:58:28 +0600

    Initialized Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        This is footer
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'footer',
+    data() {
+
+    }
+}
+</script>
+
+<style scoped>
+div {
+    height: 30px;
+    text-align: center;
+    background: teal;
+}
+</style>
