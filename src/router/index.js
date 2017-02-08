9bb770ba1afbd3d118c1d80a4852b2e4243e6c5d
dipu-bd <dipu.sudipta@gmail.com>
2017-02-08 11:44:29 +0600

    Added components

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from 'components/Hello'
 
+import BookList from 'components/BookList'
+import Book from 'components/Book'
+import MyList from 'components/MyList'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/books',
+      name: 'BookList',
+      component: BookList
+    },
+    {
+      path: '/view/:id',
+      name: 'Book',
+      component: Book
     }
   ]
 })
