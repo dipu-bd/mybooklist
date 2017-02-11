5b80cb012514105ec88ad004a71fa62f64ce6d6d
dipu-bd <dipu.sudipta@gmail.com>
2017-02-11 14:24:28 +0600

    Centered home content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
-    <div> Homepage </div>
+  <div id="home-page">
+    <div> 
+      <h1>My Book List</h1>
+      <router-link to='/books'>
+        <button>List of Books</button>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +16,11 @@ export default {
 </script>
 
 <style scoped>
+#home-page {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
 </style>
